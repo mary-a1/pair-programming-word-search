3753c7a073bf293db30dc712f6660031d637b933
Maryan <maryanay2000@gmail.com>
2023-01-18 18:14:35 -0500

    Adding paired programming files

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,22 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let element of horizontalJoin) {
+    if (element.includes(word))
+      return true;
+  }
+  for (let j = 0; j < letters.length; j++) {
+    const verticalArray = [];
+    for (let i = 0; i < letters.length; i++) {
+      if (letters[i][j]) {
+        verticalArray.push(letters[i][j]);
+      }
     }
-}
+    const lettersVertical = verticalArray.join('');
+    if (lettersVertical.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
